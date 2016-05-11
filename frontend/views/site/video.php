c4ca72b6c53402f077ffa98b75b67444b95a5d0c
bobroid <n.gilko@gmail.com>
2016-05-11 21:23:52 +0300

    Изменения страницы видео * Небольшие косметические изменения на странице видео

--- frontend/views/site/video.php
+++ frontend/views/site/video.php
@@ -1,4 +1,25 @@
 <?php
+
+/** @var \common\models\Video $video */
+
+use rmrevin\yii\fontawesome\FA;
+
+\rmrevin\yii\fontawesome\cdn\AssetBundle::register($this);
+
+$css = <<<'CSS'
+.likes-dislikes span{
+    display: block;
+}
+
+.likes-dislikes span, .likes-dislikes span i{
+    line-height: 30px;
+    vertical-align: bottom;
+}
+CSS;
+
+$this->registerCss($css);
+
+$this->title = $video->name.' - Youtuber';
 /**
  * Created by PhpStorm.
  * User: bobroid
@@ -7,15 +28,30 @@
  * @var \frontend\models\YoutubeVideo $video
  */
 
-echo \yii\bootstrap\Html::tag('h1', $video->name),
+echo /*\yii\bootstrap\Html::tag('h1', $video->name),
     \yii\bootstrap\Html::tag('div',
         \yii\bootstrap\Html::tag('span', 'Лайков: '.$video->likes, ['class' => '']).
         \yii\bootstrap\Html::tag('span', 'Дизлайков: '.$video->dislikes, ['class' => '']).
         \yii\bootstrap\Html::tag('span', 'Просмотров: '.$video->views, ['class' => ''])
     ),
-    \yii\bootstrap\Html::tag('div', 'Загружено: '.$video->uploaded);
+    \yii\bootstrap\Html::tag('div', 'Загружено: '.$video->uploaded);*/'';
 
 ?>
-<div class="col-xs-12">
-    <iframe width="1080" height="520" src="https://www.youtube.com/embed/<?=preg_replace('/(.*)\?v=/', '', $video->link)?>" frameborder="0" allowfullscreen></iframe>
+<div class="row" style="margin-top: 20px">
+    <div class="col-xs-10 col-lg-offset-1">
+        <div class="col-xs-12">
+            <h4><?=$video->name?></h4>
+        </div>
+        <div class="col-xs-12">
+            <iframe width="100%" height="520" src="https://www.youtube.com/embed/<?=preg_replace('/(.*)\?v=/', '', $video->link)?>" frameborder="0" allowfullscreen></iframe>
+        </div>
+        <div class="col-xs-6">
+            <div class="likes-dislikes">
+                <span><?=FA::i('upload', ['title' => 'Загружено'])->size(FA::SIZE_2X)?>&nbsp;Загружено:&nbsp;<?=\Yii::$app->formatter->asDate($video->uploaded, 'php:d.m.Y')?>&nbsp;</span>
+                <span><?=FA::i('eye', ['title' => 'Просмотров'])->size(FA::SIZE_2X)?>&nbsp;Просмотров:&nbsp;<?=number_format($video->views, 0, '.', ' ')?>&nbsp;</span>
+                <span><?=FA::i('thumbs-o-up', ['title' => 'Лайков'])->size(FA::SIZE_2X)?>&nbsp;Понравилось:&nbsp;<?=number_format($video->likes, 0, '.', ' ')?>&nbsp;</span>
+                <span><?=FA::i('thumbs-o-down', ['title' => 'Дизлайков'])->size(FA::SIZE_2X)?>&nbsp;Не понравилось:&nbsp;<?=number_format($video->dislikes, 0, '.', ' ')?>&nbsp;</span>
+            </div>
+        </div>
+    </div>
 </div>
