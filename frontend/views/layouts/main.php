faf41bfc6165f04067b826e82c23ceb218151862
bobroid <n.gilko@gmail.com>
2016-05-12 15:16:18 +0300

    favicon added

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -9,6 +9,23 @@ use yii\bootstrap\NavBar;
 use yii\widgets\Breadcrumbs;
 use frontend\assets\AppAsset;
 use common\widgets\Alert;
+$this->registerLinkTag(['rel' => 'apple-touch-icon', 'sizes' => '57x57', 'href' => '/apple-icon-57x57.png']);
+$this->registerLinkTag(['rel' => 'apple-touch-icon', 'sizes' => '60x60', 'href' => '/apple-icon-60x60.png']);
+$this->registerLinkTag(['rel' => 'apple-touch-icon', 'sizes' => '72x72', 'href' => '/apple-icon-72x72.png']);
+$this->registerLinkTag(['rel' => 'apple-touch-icon', 'sizes' => '76x76', 'href' => '/apple-icon-76x76.png']);
+$this->registerLinkTag(['rel' => 'apple-touch-icon', 'sizes' => '114x114', 'href' => '/apple-icon-114x114.png']);
+$this->registerLinkTag(['rel' => 'apple-touch-icon', 'sizes' => '120x120', 'href' => '/apple-icon-120x120.png']);
+$this->registerLinkTag(['rel' => 'apple-touch-icon', 'sizes' => '144x144', 'href' => '/apple-icon-144x144.png']);
+$this->registerLinkTag(['rel' => 'apple-touch-icon', 'sizes' => '152x152', 'href' => '/apple-icon-152x152.png']);
+$this->registerLinkTag(['rel' => 'apple-touch-icon', 'sizes' => '180x180', 'href' => '/apple-icon-180x180.png']);
+$this->registerLinkTag(['rel' => 'icon', 'sizes' => '192x192', 'href' => '/android-icon-192x192.png']);
+$this->registerLinkTag(['rel' => 'icon', 'sizes' => '32x32', 'href' => '/favicon-32x32.png']);
+$this->registerLinkTag(['rel' => 'icon', 'sizes' => '96x96', 'href' => '/favicon-96x96.png']);
+$this->registerLinkTag(['rel' => 'icon', 'sizes' => '16x16', 'href' => '/favicon-16x16.png']);
+$this->registerLinkTag(['rel' => 'manifest', 'href' => '/manifest.json']);
+$this->registerMetaTag(['name' => 'msapplication-TileColor', 'content'  =>  '#0c84e4']);
+$this->registerMetaTag(['name' => 'msapplication-TileImage', 'content'  =>  '/ms-icon-144x144.png']);
+$this->registerMetaTag(['name' => 'theme-color', 'content'  =>  '#0c84e4']);
 
 AppAsset::register($this);
 ?>
