afe8c6f627ade48f8ebf540e9b0988a3f9d07cb3
BoBRoID <n.gilko@gmail.com>
2016-05-15 17:17:39 +0300

    debug

--- console/migrations/m160515_140810_drop_link_index.php
+++ console/migrations/m160515_140810_drop_link_index.php
@@ -13,7 +13,7 @@ class m160515_140810_drop_link_index extends Migration
     public function up()
     {
         //$this->dropColumn(\common\models\Link::tableName(), 'index');
-        $this->addPrimaryKey('PRIMARY', \common\models\Link::tableName(), 'youtubeID');
+        $this->addPrimaryKey('youtubeID', \common\models\Link::tableName(), 'youtubeID');
     }
 
     /**
