2867a37f697099e7c19e5374464a9f3d5a9b1b33
BoBRoID <n.gilko@gmail.com>
2016-05-15 14:26:44 +0300

    migrations_fix

--- console/migrations/m160514_155429_drop_bad_keys.php
+++ console/migrations/m160514_155429_drop_bad_keys.php
@@ -15,7 +15,7 @@ class m160514_155429_drop_bad_keys extends Migration
         $this->dropIndex('link', \common\models\Link::tableName());
         //$this->dropPrimaryKey('index', \common\models\Link::tableName());
         //$this->addPrimaryKey('youtubeID', \common\models\Link::tableName(), 'youtubeID');
-        $this->createIndex('youtubeID', \common\models\Link::tableName(), 'youtubeID', true);
+        //$this->createIndex('youtubeID', \common\models\Link::tableName(), 'youtubeID', true);
         $this->createIndex('checked', \common\models\Video::tableName(), 'checked');
     }
 
