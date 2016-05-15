cac15c749a8f59ebdc0a039e8812dcfd4d79f140
BoBRoID <n.gilko@gmail.com>
2016-05-15 17:16:57 +0300

    debug

--- console/migrations/m160515_140810_drop_link_index.php
+++ console/migrations/m160515_140810_drop_link_index.php
@@ -12,9 +12,8 @@ class m160515_140810_drop_link_index extends Migration
      */
     public function up()
     {
-        $this->dropColumn(\common\models\Link::tableName(), 'index');
-        //$this->dropPrimaryKey('index', \common\models\Link::tableName());
-        //$this->addPrimaryKey('PRIMARY', \common\models\Link::tableName(), 'youtubeID');
+        //$this->dropColumn(\common\models\Link::tableName(), 'index');
+        $this->addPrimaryKey('PRIMARY', \common\models\Link::tableName(), 'youtubeID');
     }
 
     /**
