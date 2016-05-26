d870b5a982b3759a85585cb5b686be3175f7fc8f
BoBRoID <n.gilko@gmail.com>
2016-05-26 10:34:00 +0300

    fix update time

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -4,7 +4,7 @@
 
 
 $js = <<<'JS'
-setInterval(function(){if($("#autoUpdateCheckbox").prop('checked')){$.pjax.reload({container: '#last-added', timeout: '5000'});}}, 1500);
+setInterval(function(){if($("#autoUpdateCheckbox").prop('checked')){$.pjax.reload({container: '#last-added', timeout: '5000'});}}, 2500);
 JS;
 
 $this->registerJs($js);
